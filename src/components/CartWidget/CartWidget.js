198451335f016cf7c1776f914c38af9ba5a0fc10
Barbara Baciocco <barbarabacioccoo@gmail.com>
2024-02-04 00:48:31 -0300

    3

--- src/components/CartWidget/CartWidget.js
+++ src/components/CartWidget/CartWidget.js
@@ -1,9 +1,10 @@
 import cart from './assets/cart.png'
+import './CartWidget.css'
 
 const CartWidget = () => {
     return (
         <div>
-            <img src={cart} alt="cart-wdiget"/>
+            <img src={cart} alt="cart-wdiget" className="cart-img"/>
             0
         </div>
     )
